daceff5b5c8d8e111184f5a03b1f7f2f1f5ec6bb
Sourabh Shinde <saurabh@Saurabhs-MacBook-Air.local>
2024-07-14 19:30:12 +0530

    USE GSI Index into DyamoDB

--- DynamoDB_Lab/DynamoDB_Data_ops.py
+++ DynamoDB_Lab/DynamoDB_Data_ops.py
@@ -2,10 +2,11 @@ import boto3
 import os
 import random
 from pynamodb.models import Model
+from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
+
 from pynamodb.attributes import *
 from dotenv import load_dotenv
 load_dotenv()
-
 from faker import Faker
 
 fake =Faker()  # Fake Data Library
@@ -19,17 +20,45 @@ class UserModel(Model):
         aws_private_key_id = os.getenv("AWS_SECRET_KEY")
     
     user_id = UnicodeAttribute(hash_key = True)
-    order_id = UnicodeAttribute(range_key = True)
+    product_id = UnicodeAttribute(range_key = True)
     first_name = UnicodeAttribute(null = True)
     last_name = UnicodeAttribute(null = True)
 
-def main():
+def query():
+    for user in UserModel.query("2", UserModel.order_id.between("20", "40")):
+        # print("in query")
+        print(user.first_name, user.last_name, user.order_id, user.user_id)
 
-    print(f"Table {os.getenv('AWS_ACCESS_KEY')}")
-    for i in range(100):
+# for GSI CHanging Meta data
+class ViewIndex(GlobalSecondaryIndex):
+    class Meta:
+        index_name = 'product_id-user_id-index'
+        projection = AllProjection
+        aws_access_key_id = os.getenv("AWS_ACCESS_KEY")
+        aws_private_key_id = os.getenv("AWS_SECRET_KEY")
+
+    user_id = UnicodeAttribute(range_key=True)
+    product_id = UnicodeAttribute(hash_key=True)
+
+# Making Model for GSI, uses in GSI Query 
+class GSI_Query_Model(Model):
+    class Meta:
+        table_name = os.getenv('TABLE_NAME')
+    user_id = UnicodeAttribute(range_key=True)
+    product_id = UnicodeAttribute(hash_key=True)
+    first_name = UnicodeAttribute(null=True)
+    last_name = UnicodeAttribute(null=True)
+
+def gsi_Query():
+    # rangekey_condition = GSI_Query_Model.product_id.between("2000", "4000")
+    for prod in GSI_Query_Model.query("2","114"):
+        print(prod.first_name, prod.last_name)
+
+def main():
+    for _ in range(100):
         response = UserModel(
-            user_id = random.randint(1,8).__str__(),
-            order_id = random.randint(10, 1000).__str__(),
+            user_id = random.randint(1,9).__str__(),
+            product_id = random.randint(1, 100).__str__(),
             first_name = fake.first_name().__str__(),
             last_name = fake.last_name().__str__(),
         ).save()
@@ -37,5 +66,6 @@ def main():
         print(response)
         
 if __name__== "__main__":
-    main()
-    
+    # main()
+    gsi_Query()
+    # query()
